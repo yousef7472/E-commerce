e461f55c255842e107ee450586776a620293b66f
yousef7472 <yousef7shaban@gmail.com>
2025-08-28 13:54:12 +0300

    feat: onclick home button from checkout or product page redirect the user to home

--- pages/home/product/product_script.js
+++ pages/home/product/product_script.js
@@ -5,15 +5,30 @@ let product_title = document.querySelector("#title");
 let product_price = document.querySelector("#price");
 let product_rating = document.querySelector("#rating");
 let product_description = document.querySelector("#description");
+let homeBtn = document.getElementById("home");
+console.log(homeBtn);
+
 const incrementButton = document.querySelector("#increment");
 const decrementButton = document.querySelector("#decrement");
 const quantityInput = document.querySelector("#quantity");
+let addToCardBtn = document.querySelector("#addToCard");
 
 incrementButton.addEventListener("click", () => {
   quantityInput.value = parseInt(quantityInput.value) + 1;
 });
 decrementButton.addEventListener("click", () => {
   quantityInput.value = parseInt(quantityInput.value) - 1;
+});
+homeBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.href = `../home.html`;
+});
+addToCardBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let storedData = localStorage.getItem("selectedProduct");
+  let res = JSON.parse(storedData);
+  window.location.href = `../checkout/checkout.html?id=${res.id}`;
 });
 
 // Get product from localStorage
@@ -23,9 +38,9 @@ if (product) {
   pathElement.textContent = `Home / ${product.category} / ${product.title}`;
   product_img.src = `${product.thumbnail}`;
   product_title.textContent = `${product.title}`;
-  product_price.textContent = `$${product.price}`;
-  product_rating.textContent = `${product.rating}`;
-  product_description.textContent = `${product.description}`;
+  product_price.textContent = `Price: $${product.price}`;
+  product_rating.textContent = `Rating: ${product.rating}`;
+  product_description.textContent = `Description: ${product.description}`;
 } else {
   document.body.innerHTML = "<p>No product found</p>";
 }
